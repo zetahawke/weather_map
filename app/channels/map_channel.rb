57f9d913912967e921af42f0461bc755ef0c866a
zetahawke <michel.szinavel@gmail.com>
2018-12-14 17:04:48 -0300

    Using react to render map component

---
+++ app/channels/map_channel.rb
@@ -0,0 +1,4 @@
+module ApplicationCable
+  class MapChannel < ApplicationCable::Channel
+  end
+end
---
+++ app/javascript/components/Welcome/Map.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+class Map extends React.Component {
+  render() {
+    // return (
+    //   <React.Fragment>
+    //   </React.Fragment>
+    // );
+    return <h1>{this.props.greeting}</h1>
+  }
+}
+
+export default Map
