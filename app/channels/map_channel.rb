54c820e37552012edd2ed80a32d27d5efc483ad6
zetahawke <michel.szinavel@gmail.com>
2018-12-14 17:08:15 -0300

    Adding channel and connecting it to subscribe and listen future map updates

--- app/channels/map_channel.rb
+++ app/channels/map_channel.rb
@@ -1,4 +1,9 @@
-module ApplicationCable
-  class MapChannel < ApplicationCable::Channel
+class MapChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from 'map_channel'
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
   end
 end
--- app/javascript/components/Welcome/Map.jsx
+++ app/javascript/components/Welcome/Map.jsx
@@ -1,10 +1,22 @@
 import React from "react"
 class Map extends React.Component {
+  componentDidMount() {
+    App.carros = App.cable.subscriptions.create("MapChannel", {
+      connected: function () {
+        console.log("Conectado")
+      },
+
+      disconnected: function () {
+        // Called when the subscription has been terminated by the server
+      },
+
+      received: function (data) {
+        console.log(data);
+      }
+    });
+  }
+
   render() {
-    // return (
-    //   <React.Fragment>
-    //   </React.Fragment>
-    // );
     return <h1>{this.props.greeting}</h1>
   }
 }
